b1df1d82c00cb9c05acc95f733f5c3fd245a2930
Mateus Brito Bitencourt <mateus.brito.bitencourt@proton.me>
2026-07-10 15:30:28 -0300

    feat(cogs/sheet): imports the necessary libraries for the cog to interact with the database.
    Co-authored-by: Gabriel Brito Bitencourt <gabriel.brito.bitencourt@proton.me>

--- cogs/sheet.py
+++ cogs/sheet.py
@@ -1,6 +1,12 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from pathlib import Path
+
+import sqlite3
+
+from utils.db_functions import get_character_profile
+from utils.db_functions import get_character_resources
 
 class Sheet(commands.Cog):
     """
@@ -9,7 +15,13 @@ class Sheet(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.bot = bot
+        project_root = Path(__file__).resolve().parent.parent
+        database_path = project_root / "data" / "database.db"
 
+        # Establishing the persistent, active connection channel
+        self.db_connection = sqlite3.connect(database_path)
+        self.db_connection.execute("PRAGMA foreign_keys = ON;")
 
 
 
